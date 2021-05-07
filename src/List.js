b09d8a1eef5f0da3d49f5d4855d9a6ecee7439f4
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-05-07 17:05:18 +0200

    ✨ Remove item

--- src/List.js
+++ src/List.js
@@ -1,18 +1,29 @@
 import { connect } from 'react-redux';
 import React from 'react';
 
-function List({ items }) {
+function List({ items, dispatch}) {
     return (
         <ul>
             {items.map((item) => {
-                return <li>{item}</li>;
+                return(
+                    <React.Fragment>
+                        <li>{item.id}: {item.text} - {new Date(item.timestamp).toLocaleTimeString()}</li>
+                        <button id={item.id} onClick={function (e){
+                            const buttonId = e.target.id;
+                            dispatch({
+                                type: "REMOVE_ITEMS",
+                                buttonId
+                            });
+                        }}>X</button>
+                    </React.Fragment>
+                );
             })}
         </ul>
     );
 }
 
-const mapStateToProps = state => {
-    return { items: state?.items === undefined ? [] : state?.items };
+const mapStateToProps = (state, {dispatch}) => {
+    return { items: state?.items === undefined ? [] : state?.items , dispatch: dispatch};
 };
 
 export default connect(mapStateToProps)(List);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,33 @@ import App from './App';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 const ADD_ITEMS = "ADD_ITEMS";
+const REMOVE_ITEMS = "REMOVE_ITEMS";
+let id = 0;
 
 
 let store = createStore(function (state, action) {
 
   switch (action.type) {
     case ADD_ITEMS:
+      id++;
       return {
         ...state,
-        items: [...state?.items === undefined ? [] : state?.items, action.value]
+        items: [...state?.items === undefined ? [] : state?.items, {id: id, text: action.value, timestamp: Date.now()}],
       }
+    case REMOVE_ITEMS:
 
+      const itemList = state?.items;
+
+      itemList.forEach(element => {
+        if(element.id === action.value){
+          itemList.remove(element);
+        }
+      });
+
+      return {
+        ...state,
+        items: [],
+      }
     default:
       return state;
   }
